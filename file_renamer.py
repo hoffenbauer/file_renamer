f16d5abd7f712d2931fb55a3feec2b02ab97ed27
Lucas <66139861+hoffenbauer@users.noreply.github.com>
2021-10-21 12:47:05 -0300

    Add files via upload

--- file_renamer.py
+++ file_renamer.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-extension = input("Choose extension of files: ")
-prefix = input ("Choose file prefix: ")
-x = 0
+import os
+import platform
 
-for file in os.listdir():
-    file = file.lower()
-    if file.endswith(f'.{extension.lower()}'):
-        x += 1
-        os.rename(file, f'{prefix}_{x}.{extension.lower()}')
-        
+def file_renamer():
+    path = input("Path to the files to be renamed: ")
+    extension = input("Choose extension of files: ")
+    prefix = input ("Choose file prefix: ")
+    x = 0
+
+    if platform.system() == 'Windows':
+        os.chdir(fr'{path}')
+    else:
+        os.chdir(path)
+
+    for file in os.listdir():
+        file = file.lower()
+        if file.endswith(f'.{extension.lower()}'):
+            x += 1
+            os.rename(file, f'{prefix}_{x}.{extension.lower()}')
 
